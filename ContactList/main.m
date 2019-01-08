8e3d9f09690ce348fd19672cebe2069eb512a069
SpencerSymington <theSpencerSymington@gmail.com>
2019-01-08 08:58:33 -0800

    init commit

---
+++ ContactList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ContactList
+//
+//  Created by Spencer Symington on 2019-01-08.
+//  Copyright © 2019 Penjat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return 0;
+}
